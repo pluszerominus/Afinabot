9ce208a962cbf4724d727ca6c613b0c25e57483a
legolaisik <legolas-lego@mail.ru>
2024-07-07 19:29:40 +0300

    Add model LSTM

---
+++ models/model_LSTM.py
@@ -0,0 +1,16 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Embedding, Dropout
+
+
+def model_LSTM():
+    model = Sequential()
+
+    model.add(Embedding(2048, 2, input_length=10))
+    model.add(Dropout(0.15))
+    model.add(LSTM(32, return_sequences=True))
+    model.add(LSTM(32))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
