735e0f81902ddd312eaadab5b522dac2c09c8412
legolaisik <legolas-lego@mail.ru>
2024-07-16 12:03:26 +0300

    Update LSTM model

--- models/model_LSTM.py
+++ models/model_LSTM.py
@@ -7,8 +7,7 @@ def model_LSTM():
 
     model.add(Embedding(2048, 2, input_length=10))
     model.add(Dropout(0.15))
-    model.add(LSTM(32, return_sequences=True))
-    model.add(LSTM(32))
+    model.add(LSTM(64))
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
